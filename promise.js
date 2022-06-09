95bfbfd0e0e65e663a4fd4dfa00e87028ba72e85
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-06-09 21:39:02 +0700

    Challenge 07 has been resolved

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+
+  const ixxFiltered = ixx.filter(movie => movie.hasil === emotion);
+  const vgcFiltered = vgc.filter(movie => movie.hasil === emotion);
+
+  return (ixxFiltered.length + vgcFiltered.length);
+}
 
 module.exports = {
   promiseOutput,
